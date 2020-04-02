007524188e85f114875ae70bcd5803edbf8f7e96
鲍佳玮 <bjw_0627@163.com>
2020-04-02 21:24:13 +0800

    封装reportWrapper组件，修改cube-scroll的bug

---
+++ src/views/report/components/reportWrapper.vue
@@ -0,0 +1,109 @@
+<template>
+  <div class="report-wrapper">
+    <div class="tit">
+      <div class="tit-wrapper">
+        <span>{{title}}</span>
+      </div>
+    </div>
+    <div class="content">
+      <slot name="content"></slot>
+    </div>
+    <div class="advice">
+      <div class="advice-tit">解读建议</div>
+      <p class="advice-info">{{advice}}</p>
+    </div>
+  </div>
+</template>
+
+<script type="text/ecmascript-6">
+export default {
+  props: {
+    title: {
+      type: String,
+      default: ''
+    },
+    advice: {
+      type: String,
+      default: ''
+    }
+  },
+  created () {},
+  methods: {}
+}
+</script>
+
+<style lang="stylus" rel="stylesheet/stylus">
+@import '~common/stylus/mixin';
+@import '~common/stylus/variable';
+
+.report-wrapper {
+  width: 100%;
+  box-sizing: border-box;
+  margin-bottom: 40px;
+
+  .tit {
+    width: 100%;
+    position: relative;
+
+    > ::before {
+      content: ' ';
+      width: 8px;
+      height: 32px;
+      background: rgba(0, 81, 255, 1);
+      position: absolute;
+      top: 50%;
+      transform: translateY(-50%);
+      left: 40px;
+    }
+
+    .tit-wrapper {
+      padding: 24px 40px 24px 64px;
+      box-sizing: border-box;
+      display: flex;
+      justify-content: space-between;
+
+      > span:last-child {
+        font-size: 36px;
+        font-family: PingFangSC-Medium, PingFang SC;
+        color: rgba(43, 45, 48, 1);
+        font-weight: 500;
+      }
+    }
+  }
+
+  .content {
+    margin: 22px 32px 40px 32px;
+  }
+
+  .advice {
+    margin: 0 32px;
+    background: rgba(243, 243, 246, 1);
+    border-radius: 4px;
+    position: relative;
+
+    .advice-tit {
+      width: 130px;
+      height: 44px;
+      line-height: 44px;
+      text-align: center;
+      background: url('~images/detail/bg-tip.png') no-repeat;
+      background-size: cover;
+      font-size: 24px;
+      font-family: PingFangSC-Medium;
+      font-weight: 500;
+      color: rgba(255, 255, 255, 1);
+      position: absolute;
+      top: -22px;
+    }
+
+    .advice-info {
+      font-size: 24px;
+      font-family: PingFangSC-Regular;
+      font-weight: 400;
+      color: rgba(102, 102, 102, 1);
+      line-height: 36px;
+      padding: 44px 28px 26px 28px;
+    }
+  }
+}
+</style>
--- src/views/report/detail.vue
+++ src/views/report/detail.vue
@@ -1,166 +1,95 @@
 <template>
-  <div class="creditAnalysis-wrapper">
+  <div class="report-detail-wrapper">
     <nav-bar :title="title"></nav-bar>
-    <div class="header">
-      <div class="new-flash-warpper">
-        <i class="icon-new-flash"></i>
-        <div class="slide-loan-list">
-          <cube-slide :data="loanCustList" direction="vertical" :showDots="false" :interval="2000">
-            <cube-slide-item v-for="(item, index) in loanCustList" :key="index">
-              <div class="slide-item">
-                <span>{{item.name}}完成检测优化后，借款成功</span>
-                <span class="randTime">{{item.randTime}}分钟前</span>
+    <div class="detail-wrapper">
+      <cube-scroll>
+        <div class="header">
+          <div class="user-info-wrapper">
+            <div class="avatar"></div>
+            <div class="info">
+              <div class="name">**梅</div>
+              <div class="additional">
+                <div>
+                  <label>手机号码</label>
+                  <span>135****8888</span>
+                </div>
+                <div>
+                  <label>身份证号</label>
+                  <span>320*************88</span>
+                </div>
+                <div>
+                  <label>评估时间</label>
+                  <span>2019-06-24 00:00:00</span>
+                </div>
               </div>
-            </cube-slide-item>
-          </cube-slide>
+            </div>
+          </div>
         </div>
-      </div>
+        <report-wrapper
+          :title="reportWrapperTextArr[0].title"
+          :advice="reportWrapperTextArr[0].advice"
+        >
+          <template name="content">13231312</template>
+        </report-wrapper>
+        <report-wrapper
+          :title="reportWrapperTextArr[1].title"
+          :advice="reportWrapperTextArr[1].advice"
+        >
+          <template name="content">13231312</template>
+        </report-wrapper>
+        <report-wrapper
+          :title="reportWrapperTextArr[2].title"
+          :advice="reportWrapperTextArr[2].advice"
+        >
+          <template name="content">13231312</template>
+        </report-wrapper>
+        <report-wrapper
+          :title="reportWrapperTextArr[3].title"
+          :advice="reportWrapperTextArr[3].advice"
+        >
+          <template name="content">13231312</template>
+        </report-wrapper>
+      </cube-scroll>
     </div>
-    <div class="input-wrapper">
-      <div class="validator-item">
-        <cube-input
-          v-model="inputProps[0].text"
-          :placeholder="inputProps[0].placeholder"
-          :type="inputProps[0].type"
-          :maxlength="inputProps[0].maxlength"
-          :clearable="true"
-          @blur="fixIosBug"
-          ref="idCardName"
-        >
-          <template slot="prepend">姓名</template>
-        </cube-input>
-        <cube-input
-          v-model="inputProps[1].text"
-          :placeholder="inputProps[1].placeholder"
-          :type="inputProps[1].type"
-          :maxlength="inputProps[1].maxlength"
-          :clearable="true"
-          @blur="fixIosBug"
-          ref="idCardCode"
-        >
-          <template slot="prepend">身份证</template>
-        </cube-input>
-        <cube-input
-          v-model="inputProps[2].text"
-          :placeholder="inputProps[2].placeholder"
-          :type="inputProps[2].type"
-          :maxlength="inputProps[2].maxlength"
-          :clearable="true"
-          @blur="fixIosBug"
-          ref="telNo"
-        >
-          <template slot="prepend">手机号</template>
-        </cube-input>
-      </div>
-      <div class="choose-protocol">
-        <i class="check" @click="chooseProtocol"></i>
-        <div>
-          我已阅读并同意
-          <router-link tag="span" :to="{ name: 'reportProtocol' }">《服务协议》</router-link>
-        </div>
-      </div>
-    </div>
-    <div class="desc1"></div>
-    <div class="desc2"></div>
-    <button class="bottom-btn" @click="queryReport">立即查询</button>
-    <transition name="slide">
-      <router-view></router-view>
-    </transition>
   </div>
 </template>
 
 <script type="text/ecmascript-6">
 import navBar from 'components/navBar/navBar'
-import { getLoanCustList } from 'api/loanProduct'
-import {
-  checkPhone,
-  checkCustName,
-  checkIdCardCode
-} from 'common/js/validator'
+import reportWrapper from './components/reportWrapper'
 
 export default {
   data () {
     return {
-      title: '网贷检测',
-      loanCustList: [],
-      chooseProtocolFlag: true,
-      inputProps: [
+      title: '报告详情',
+      reportWrapperTextArr: [
         {
-          placeholder: '请输入姓名',
-          type: 'text',
-          maxlength: 11,
-          text: ''
+          title: '信用分',
+          advice: '信用分越高，代表信用越好，贷款成功率越高。'
         },
         {
-          placeholder: '请输入身份证号',
-          type: 'text',
-          maxlength: 18,
-          text: ''
+          title: '黑名单',
+          advice:
+            '高风险关注名单从身份证、手机号两个维度在我们的源高风险关注名单匹配。用户命中黑名单，贷款通过率低。'
         },
         {
-          placeholder: '请输入手机号',
-          type: 'text',
-          maxlength: 11,
-          text: ''
+          title: '运营商认证',
+          advice:
+            '贷款申请运营商认证必须验证一致，且在网状态正常，时常在12个月以上为佳。'
+        },
+        {
+          title: '法院信息',
+          advice:
+            '若主体命中失信黑名单或命中法院执行，需要积极的与法院和借款方进行沟通协商，及时将所需的欠款还清，方可成功借款。'
         }
       ]
     }
   },
-  filters: {
-    formatMoney (item) {
-      return item / 100
-    }
-  },
-  created () {
-    this._getLoanCustList()
-  },
-  methods: {
-    _getLoanCustList () {
-      const _self = this
-      getLoanCustList().then(res => {
-        _self.loanCustList = res.data
-        _self.loanCustList.forEach(item => {
-          item.randTime = Math.floor(Math.random() * 10 + 1)
-        })
-      })
-    },
-    chooseProtocol (e) {
-      if (e.target.className === 'check') {
-        e.target.classList.remove('check')
-        e.target.classList.add('uncheck')
-        this.chooseProtocolFlag = false
-      } else {
-        e.target.classList.remove('uncheck')
-        e.target.classList.add('check')
-        this.chooseProtocolFlag = true
-      }
-    },
-    queryReport () {
-      if (!this.chooseProtocolFlag) {
-        this.$createToast({
-          txt: '请勾选服务协议',
-          time: 2000,
-          type: 'warn'
-        }).show()
-      }
-      if (!checkCustName(this.inputProps[0].text, this.$refs.idCardName)) {
-        return
-      }
-      if (!checkIdCardCode(this.inputProps[1].text, this.$refs.idCardCode)) {
-        return
-      }
-      if (!checkPhone(this.inputProps[2].text, this.$refs.telNo)) {
-        return
-      }
-      console.log('submit')
-    },
-    fixIosBug () {
-      document.documentElement.scrollTop = 0
-      document.body.scrollTop = 0
-    }
-  },
+  created () {},
+  methods: {},
   components: {
-    navBar: navBar
+    navBar: navBar,
+    reportWrapper: reportWrapper
   }
 }
 </script>
@@ -169,159 +98,74 @@ export default {
 @import '~common/stylus/mixin';
 @import '~common/stylus/variable';
 
-.slide-enter-active, .slide-leave-active {
-  transition: all 0.3s;
-}
-
-.slide-enter, .slide-leave-to {
-  transform: translate3d(100%, 0, 0);
-}
-
-.creditAnalysis-wrapper {
+.report-detail-wrapper {
   position: fixed;
   top: 0;
   left: 0;
   right: 0;
   bottom: 0;
   background: #fff;
+  z-index: 2;
 
-  >.header {
+  .detail-wrapper {
+    position: fixed;
+    top: 96px;
+    left: 0;
+    right: 0;
+    bottom: 0;
+  }
+
+  .header {
     width: 100%;
-    height: 316px;
-    background: url('~images/creditAnalysis/bg-banner.png') no-repeat;
+    height: 408px;
+    background: url('~images/detail/bg-top.png') no-repeat;
     background-size: cover;
     position: relative;
 
-    .new-flash-warpper {
+    .user-info-wrapper {
       position: absolute;
-      bottom: -40px;
-      left: 50%;
-      transform: translateX(-50%);
-      width: 686px;
-      height: 80px;
-      padding-left: 36px;
-      background: rgba(255, 255, 255, 1);
-      box-shadow: 0px 2px 16px 0px rgba(152, 165, 209, 0.3);
-      border-radius: 42px;
-      box-sizing: border-box;
+      top: 76px;
+      left: 64px;
+      display: flex;
 
-      .icon-new-flash {
-        width: 36px;
-        height: 37px;
-        background-image: url('~images/creditAnalysis/broadcast.png');
-        background-size: 100% 100%;
-        display: inline-block;
-        vertical-align: middle;
+      .avatar {
+        width: 80px;
+        height: 80px;
+        background: url('~images/detail/avatar.png') no-repeat;
+        background-size: cover;
       }
 
-      .slide-loan-list {
-        width: 560px;
-        display: inline-block;
-        vertical-align: middle;
-        font-size: 28px;
-        color: #4a4a4a;
-        margin-left: 18px;
-        height: 80px;
-        line-height: 80px;
+      .info {
+        margin-left: 32px;
 
-        .slide-item {
-          display: flex;
-          align-items: center;
-          justify-content: space-between;
+        .name {
+          font-size: 44px;
+          font-family: PingFangSC-Medium, PingFang SC;
+          font-weight: 500;
+          color: rgba(43, 45, 48, 1);
+          margin-bottom: 42px;
         }
 
-        .randTime {
+        .additional {
           font-size: 28px;
           font-family: PingFangSC-Regular, PingFang SC;
           font-weight: 400;
-          color: rgba(178, 179, 181, 1);
+
+          >div {
+            margin-bottom: 28px;
+          }
+
+          label {
+            color: rgba(102, 102, 102, 1);
+          }
+
+          span {
+            margin-left: 24px;
+            color: rgba(43, 45, 48, 1);
+          }
         }
       }
     }
-  }
-
-  >.input-wrapper {
-    margin: 92px 32px 36px 32px;
-    padding: 30px 32px 48px 32px;
-    background: rgba(255, 255, 255, 1);
-    box-shadow: 0px 6px 24px 0px rgba(152, 165, 209, 0.3);
-    border-radius: 24px;
-    border: 1px solid rgba(229, 237, 250, 1);
-
-    .cube-input {
-      height: 93px;
-      line-height: 93px;
-      margin-bottom: 6px;
-    }
-
-    .cube-input::after {
-      border: 1PX solid transparent;
-      border-bottom: 1PX solid #e6e6e6;
-    }
-
-    .cube-input-field {
-      text-align: right;
-      font-size: 32px;
-      padding: 0;
-    }
-
-    .cube-input-prepend {
-      font-size: 32px;
-      font-family: PingFangSC-Medium, PingFang SC;
-      font-weight: 500;
-      color: rgba(43, 45, 48, 1);
-    }
-
-    .choose-protocol {
-      font-family: PingFangSC-Regular;
-      color: #5D5D77;
-      font-size: 0;
-      margin-top: 38px;
-
-      > .check {
-        inline-block-direction();
-        width: 24px;
-        height: 25px;
-        background: url('~images/creditAnalysis/check.png') no-repeat;
-        background-size: cover;
-      }
-
-      > .uncheck {
-        inline-block-direction();
-        width: 24px;
-        height: 25px;
-        background: url('~images/creditAnalysis/uncheck.png') no-repeat;
-        background-size: cover;
-      }
-
-      > div {
-        inline-block-direction();
-        margin-left: 12px;
-        font-size: 24px;
-
-        > span {
-          color: rgba(80, 131, 247, 1);
-        }
-      }
-    }
-  }
-
-  .desc1 {
-    border-top: 12px solid #F4F6F8;
-    border-bottom: 12px solid #F4F6F8;
-  }
-
-  >.bottom-btn {
-    width: 100%;
-    height: 98px;
-    background: linear-gradient(90deg, rgba(57, 157, 250, 1) 0%, rgba(105, 102, 243, 1) 100%);
-    position: fixed;
-    bottom: 0;
-    z-index: 1;
-    font-size: 40px;
-    font-family: PingFangSC-Semibold, PingFang SC;
-    font-weight: 600;
-    color: rgba(255, 255, 255, 1);
   }
 }
 </style>
