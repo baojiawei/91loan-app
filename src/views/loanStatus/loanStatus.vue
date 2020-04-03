d26f85a536a4d18a8439293c66132f35a1be447c
鲍佳玮 <bjw_0627@163.com>
2020-04-03 23:08:13 +0800

    完成loanStatus

--- src/views/loanStatus/loanStatus.vue
+++ src/views/loanStatus/loanStatus.vue
@@ -1,11 +1,22 @@
 <template>
   <div class="loanStatus-wrapper">
     <nav-bar :title="title"></nav-bar>
-    <cube-scroll>
-      <div class="loan-comment">
-        <loan-comment weight="3"></loan-comment>
-      </div>
-    </cube-scroll>
+    <div class="scroll-wrapper">
+      <cube-scroll>
+        <div class="loanStatus">
+          <img :src="statusText.imgUrl" alt />
+          <div class="status-text">{{statusText.text}}</div>
+          <div class="status-tip">{{statusText.tip}}</div>
+          <button>
+            <span>查看我的综合信用分</span>
+            <img src="~images/loanStatus/right-arrow.png" alt />
+          </button>
+        </div>
+        <div class="loan-comment">
+          <loan-comment weight="3"></loan-comment>
+        </div>
+      </cube-scroll>
+    </div>
   </div>
 </template>
 
@@ -16,10 +27,24 @@ import loanComment from 'components/loanComment/loanComment'
 export default {
   data () {
     return {
-      title: '贷款申请'
+      title: '贷款申请',
+      statusTextGroup: [{
+        imgUrl: require('@/assets/images/loanStatus/failed.png'),
+        text: '审核未通过',
+        tip: '审核不通过，快来看看是不是信用分出了问题'
+      }, {
+        imgUrl: require('@/assets/images/loanStatus/applying.png'),
+        text: '正在审核中...',
+        tip: '申请成功，请耐心等待'
+      }],
+      statusText: {}
     }
   },
-  created () {},
+  created () {
+    // 根据url传入的status，顶部显示不同状态
+    const status = this.$route.query.s
+    status && (this.statusText = this.statusTextGroup[status])
+  },
   methods: {},
   components: {
     navBar: navBar,
@@ -38,8 +63,69 @@ export default {
   left: 0;
   right: 0;
   bottom: 0;
-  background: #fff;
+  background: rgba(243, 243, 246, 1);
   z-index: 2;
+
+  .scroll-wrapper {
+    position: fixed;
+    top: 98px;
+    left: 0;
+    right: 0;
+    bottom: 0;
+  }
+
+  .loanStatus {
+    margin: 44px 32px 50px 32px;
+    display: flex;
+    align-items: center;
+    flex-direction: column;
+    justify-content: space-between;
+    height: 400px;
+    padding: 30px 30px 46px 30px;
+    background: rgba(255, 255, 255, 1);
+    box-shadow: 0px 6px 21px 0px rgba(152, 165, 209, 0.3);
+    border-radius: 24px;
+    text-align: center;
+
+    > img {
+      width: 176px;
+      height: 158px;
+    }
+
+    >.status-text {
+      font-size: 36px;
+      font-family: PingFangSC-Medium, PingFang SC;
+      font-weight: 500;
+      color: rgba(18, 33, 62, 1);
+    }
+
+    >.status-tip {
+      font-size: 28px;
+      font-family: PingFangSC-Regular, PingFang SC;
+      font-weight: 400;
+      color: rgba(144, 151, 163, 1);
+    }
+
+    > button {
+      width: 420px;
+      height: 80px;
+      border-radius: 48px;
+      border: 2px solid rgba(254, 110, 61, 1);
+
+      > span {
+        margin-right: 10px;
+        font-size: 34px;
+        font-family: PingFangSC-Medium, PingFang SC;
+        font-weight: 500;
+        color: rgba(254, 110, 61, 1);
+      }
+
+      > img {
+        width: 24px;
+        height: 24px;
+      }
+    }
+  }
 
   .loan-comment {
     margin: 44px 32px 50px 32px;
